9410b6ad9b572ea5aa7e812a035cccb9742f440c
Yubo Wu <randomalexwu@gmail.com>
2024-06-09 17:53:06 +1200

    nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './global.css'
+import { Navbar } from './components/nav'
 
 export const metadata = {
   title: "Yubo's Website",
@@ -6,13 +7,14 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html 
       lang="en"
       className="text-black bg-warm-color"
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Navbar/>
           {children}
         </main>
       </body>
