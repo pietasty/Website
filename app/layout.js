9bd9b19cd76e174bc646debe123c4d9f1ae3d5f8
Yubo Wu <randomalexwu@gmail.com>
2024-06-09 15:23:45 +1200

    apply new background colour

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
  return (
     <html 
       lang="en"
-      className="text-black bg-white dark:text-white dark:bg-black"
+      className="text-black bg-warm-color"
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
