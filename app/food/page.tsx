4d8e0f759a9c8074f3e9645d27d67aedf8a8cc80
Yubo Wu <randomalexwu@gmail.com>
2024-06-09 18:40:01 +1200

    Adding Food rating Metrics

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -2,11 +2,13 @@ export default function Page(){
   return (
     <section>
       <h1 className="mb-8 text-4xl font-semibold">
-        Food Suggestions
+        Food Suggestion
       </h1>
+      <p>Yubo's Metrics:</p>
       <ul className="list-disc">
-        <li>Food</li>
-        <li>Planes</li>
+        <li><b>Fire:</b> Very good and highly recommended by me</li>
+        <li><b>Good:</b> Tastes exactly like what you would expect when you look at the menu. Nothing special</li>
+        <li><b>Ok:</b> Won't go back. Not worth the price, could go somewhere else</li>
       </ul>
     </section>
   )
