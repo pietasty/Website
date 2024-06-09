0c24eb5d2abd9c782a813be13e7413f8218a73b6
Yubo Wu <randomalexwu@gmail.com>
2024-06-09 17:53:49 +1200

    home page change

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ export default function Page(){
   return (
     <section>
       <h1 className="mb-8 text-4xl font-semibold">
-        Yubo's Website
+        Interests
       </h1>
-      <h4>Interests</h4>
       <ul className="list-disc">
         <li>Food</li>
         <li>Planes</li>
