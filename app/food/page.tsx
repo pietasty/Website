7b902b54188bf47d3e50abb0bb0fadd915876c2f
Yubo Wu <randomalexwu@gmail.com>
2024-06-09 18:28:46 +1200

    Added a food page

---
+++ app/food/page.tsx
@@ -0,0 +1,13 @@
+export default function Page(){
+  return (
+    <section>
+      <h1 className="mb-8 text-4xl font-semibold">
+        Food Suggestions
+      </h1>
+      <ul className="list-disc">
+        <li>Food</li>
+        <li>Planes</li>
+      </ul>
+    </section>
+  )
+}
