ae59b9b796f1d569cf3050b779a3d4ac7968d916
Yubo Wu <randomalexwu@gmail.com>
2024-06-09 16:37:37 +1200

    fun page not found

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+export default function NotFound() {
+    return (
+      <section>
+        <h1 className="mb-8 text-2xl font-semibold tracking-tighter text-center">
+          404 - Plane Not Found
+        </h1>
+        <Image 
+          src="/error404.jpg" 
+          width={1200} 
+          height={900}
+          alt="test"
+          />
+        <p className="mb-4 text-xs">
+          Ontario Airport, California (ONT)
+        </p>
+      </section>
+    )
+  }
