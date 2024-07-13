a96f9ba7b785aa582391ad9e8f6ce22ba88f6b72
Yubo Wu <randomalexwu@gmail.com>
2024-07-14 11:38:04 +1200

    change max width of page

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
             lang="en"
             className="text-black bg-warm-color"
         >
-            <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+            <body className="antialiased max-w-screen-lg mx-4 mt-8 lg:mx-auto">
                 <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
                     <Navbar />
                     {children}
