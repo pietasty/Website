5006995bcb89047cd59680e2627bfe905cd425db
Yubo Wu <randomalexwu@gmail.com>
2024-07-24 21:28:57 +1200

    Added order lists

--- app/components/lists.tsx
+++ app/components/lists.tsx
@@ -1,9 +1,38 @@
+import Link from "next/link"
 
+export type ListInfo = {
+    text: string,
+    link: string
+}
 
 export function UnstyledList({list}: {list: string[]}){
     return (
-        <ul className="list-none list-inside divide-y divide-gray-400">
+        <ul className="list-none list-inside divide-y divide-gray-400 space-y-1">
             {list.map(s => <li>{s}</li>)}
         </ul>
+    )
+}
+
+export function OrderedList({list}:{list: string[]}){
+    return (
+        <ol className="list-decimal list-inside space-y-1">
+            {
+                list.map(l => <li>{l}</li>)
+            }
+        </ol>
+    )
+}
+
+export function OrderedLinkList({list}:{list: ListInfo[]}){
+    return (
+        <ol className="list-decimal list-inside space-y-1">
+            {
+                list.map(l => {
+                    return <li>
+                        <Link href={l.link}>{l.text}</Link>
+                    </li>
+                })
+            }
+        </ol>
     )
 }
